216ad0a3ac0b837a16fae0dcfc7ab7c567c7cb90
leandrosimonato <leandrosimonato@gmail.com>
2021-11-26 18:21:35 -0300

    Demonstrando os detalhes da marmita.

--- src/app/packages/package-list/package-list.component.ts
+++ src/app/packages/package-list/package-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from './package.model';
 
 @Component({
   selector: 'app-package-list',
   templateUrl: './package-list.component.html',
-  styleUrls: ['./package-list.component.css']
+  styleUrls: ['./package-list.component.css'],
 })
 export class PackageListComponent implements OnInit {
+  packages: Package[] = [
+    new Package(
+      'Churrasco',
+      'Churrasco de Picanha',
+      'https://www.receiteria.com.br/wp-content/uploads/receitas-de-churrasco-0.png'
+    ),
+    new Package(
+      'Churrasco',
+      'Churrasco de Maminha no Alho',
+      'https://img.itdg.com.br/tdg/images/recipes/000/140/822/173354/173354_original.jpg?mode=crop&width=710&height=400'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
